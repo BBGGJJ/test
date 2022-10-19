8fa4701ea65e1774e10858966ac3bfeeeee989bb
16863 <1686330573@qq.com>
2022-10-19 18:43:37 +0800

    wu

---
+++ src/main/java/persion.java
@@ -0,0 +1,12 @@
+/**
+ * @program: test
+ * @ClassName: persion
+ * @Version 1.0
+ * @author: 龚宝金
+ * @create: 2022-10-19 18:42
+ **/
+public class persion {
+    public static void main(String[] args) {
+        System.out.println("我是你爹");
+    }
+}
